b0072c3908f592ebc39c44edb4a98815995823ec
sanyog <82366234+sanyoog@users.noreply.github.com>
2026-02-11 06:10:41 +0000

    fix: Remove stray parenthesis from LinearCameraSlider.kt
    Fixed syntax error where ')' character was prepended to 'package' statement

--- app/src/main/kotlin/com/retrocam/app/ui/components/LinearCameraSlider.kt
+++ app/src/main/kotlin/com/retrocam/app/ui/components/LinearCameraSlider.kt
@@ -1,4 +1,4 @@
-)package com.retrocam.app.ui.components
+package com.retrocam.app.ui.components
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
